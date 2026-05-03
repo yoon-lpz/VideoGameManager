fed8cc6ec24b771cb5903a2cf6a6d534613c9d28
Yoon <yoon.lopez.7e9@itb.cat>
2026-05-03 12:18:20 +0200

    Visual fixings

--- Pages/Games/Details.cshtml.cs
+++ Pages/Games/Details.cshtml.cs
@@ -7,6 +7,7 @@ namespace VideoGameManager.Pages.Games
 {
     public class DetailsModel : PageModel
     {
+        public int EmptyStars, FilledStars;
         private readonly GameService _gameService;
         public Game Game { get; set; }
 
@@ -18,6 +19,8 @@ namespace VideoGameManager.Pages.Games
         public void OnGet(int id)
         {
             Game = _gameService.GetById(id);
+            FilledStars = (int) Game.Score / 2;
+            EmptyStars = 5 - FilledStars;
         }
     }
 }
