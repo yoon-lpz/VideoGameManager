d0ebdbdf880ea3ec3ca40be33034e33f432ce764
Yoon <yoon.lopez.7e9@itb.cat>
2026-05-03 18:59:16 +0200

    Configured GamesExporter, refs #15

--- Services/GamesExporter.cs
+++ Services/GamesExporter.cs
@@ -1,6 +1,26 @@
-﻿namespace VideoGameManager.Services
+﻿using CsvHelper;
+using System.Globalization;
+using System.Text;
+using VideoGameManager.Models;
+
+namespace VideoGameManager.Services
 {
     public class GamesExporter
     {
+        public byte[] ExportToCsv(List<Game> games)
+        {
+            using (var memoryStream = new MemoryStream())
+            {
+                using (var writer = new StreamWriter(memoryStream, Encoding.UTF8))
+                {
+                    using (var csvWriter = new CsvWriter(writer, CultureInfo.InvariantCulture))
+                    {
+                        csvWriter.WriteRecords(games);
+                    }
+                }
+
+                return memoryStream.ToArray();
+            }
+        }
     }
 }
